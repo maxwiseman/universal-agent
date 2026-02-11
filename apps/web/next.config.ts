9d4de3ba1d008f031cd6c2dc76e407beb81cab3c
maxwiseman <max@maxwiseman.io>
2026-02-11 10:59:13 -0500

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@universal-agent/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
